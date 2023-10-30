24692de89225ae25898c7c310f538252133efdc7
KingOreo2017 <47092117+KingOreo2017@users.noreply.github.com>
2023-10-30 09:05:33 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
           >
             By{' '}
             <Image
-              src="/download.svg"
+              src="/download.jfif"
               alt="Vercel Logo"
               className="dark:invert"
               width={100}
