1d22b03d820bd65640c3a344b1c33e22d3451c21
KingOreo2017 <47092117+KingOreo2017@users.noreply.github.com>
2023-10-31 08:41:33 -0400

    move nav into p

--- app/page.tsx
+++ app/page.tsx
@@ -105,10 +105,10 @@ export default function Home() {
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-          <nav>
+            <nav>
             <a class = "button" href="buttontest.tsx">Hello World</a>
           </nav>
+          </p>
         </a>
       </div>
     </main>
