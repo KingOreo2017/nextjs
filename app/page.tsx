e2b838122f3b7b74afe021ddc90163fe64de3943
KingOreo2017 <47092117+KingOreo2017@users.noreply.github.com>
2023-10-31 22:34:21 -0400

    fix directory issue v6

--- app/page.tsx
+++ app/page.tsx
@@ -105,9 +105,8 @@ export default function Home() {
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             Instantly deploy your Next.js site to a shareable URL with Vercel.
-          <a href="https://github.com/KingOreo2017/nextjs/raw/main/app/buttontest.tsx" target="_blank">View buttontest.tsx>
+          <a href="https://github.com/KingOreo2017/nextjs/raw/main/app/buttontest.tsx" target="_blank">View buttontest.tsx</a>
             <div className = "button">Hello World</div>
-          </a>
           </p>
         </a>
       </div>
