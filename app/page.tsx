78e7fedd98218574eaf50c8689f7e1dbd52af55c
KingOreo2017 <47092117+KingOreo2017@users.noreply.github.com>
2023-11-01 21:41:24 -0400

    bickel bigger

--- app/page.tsx
+++ app/page.tsx
@@ -32,8 +32,8 @@ export default function Home() {
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
           src="/Bickel.png"
           alt="bowl cut"
-          width={180}
-          height={37}
+          width={540}
+          height={111}
           priority
         />
       </div>
