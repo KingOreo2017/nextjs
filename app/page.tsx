b809bf000dd44026558e81413ff4f351655194cc
KingOreo2017 <47092117+KingOreo2017@users.noreply.github.com>
2023-11-06 08:40:21 -0500

    move text box

--- app/page.tsx
+++ app/page.tsx
@@ -54,13 +54,6 @@ export default function Home() {
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             This is a test. Hello World.
           </p>
-          <p>
-            <input
-              type="text"
-              className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-              placeholder="Enter text here"
-            />
-          </p>
         </a>
 
         <a
@@ -117,7 +110,11 @@ export default function Home() {
           <a href="/buttontest.htm" target="_blank">Hello World</a>
         </p>
         <p>
-          
+          <input
+            type="text"
+            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+            placeholder="Enter text here"
+          />
         </p>
       </div>
     </main>
